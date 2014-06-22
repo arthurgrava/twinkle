e791053c0c5149d2a15dae02e5dc9e4dd9ad2b1f
Arthur Grava <arthur@luizalabs.com>
2014-06-22 20:09:34 -0300

    JSON structure changed

--- src/main/java/org/goodfellas/controller/GraphPrinter.java
+++ src/main/java/org/goodfellas/controller/GraphPrinter.java
@@ -14,53 +14,65 @@ import org.goodfellas.structure.Vertex;
 import org.goodfellas.util.Constants;
 
 public class GraphPrinter {
-    
+
     public void toJson(Stack<Edge> path, Graph graph) throws IOException {
         final String fileName = "graph.json";
-        
+
         double maxX = 0.0;
         double maxY = 0.0;
         Map<Integer, Vertex> map = graph.getVertices();
         Set<Integer> keys = map.keySet();
-        
+
         for(Integer key : keys) {
             maxX = (maxX > map.get(key).getX()) ? maxX : map.get(key).getX();
             maxY = (maxY > map.get(key).getY()) ? maxY : map.get(key).getY();
         }
-        
+
         BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName), false));
-        bw.append("var graph = { \"nodes\":[");
         
-        Vertex current = null;
+        Vertex from = null;
+        Vertex to = null;
+        double fromX = -1.0;
+        double fromY = -1.0;
+        double toX = -1.0;
+        double toY = -1.0;
+
+        bw.append("var graph = { \"edges\":[");
+
         for(Integer key : keys) {
-            current = map.get(key);
-            double x = current.getX() / maxX * 960.0;
-            double y = current.getY() / maxY * 500.0;
-            
-            bw.append("{\"id\":\"" + current.getId() + "\", \"x\":" + x + ",\"y\":" + (-(y - 500)) + "},");
-        }
-        
-        bw.append("], \"links\":[");
-        
-        Vertex adjacent = null;
-        for(Integer key : keys) {
-            current = map.get(key);
+            from = map.get(key);
+            fromX = from.getX() / maxX * 960.0;
+            fromY = from.getY() / maxY * 500.0;
+
             // just to remember from = current and to = adjacent
-            for(Edge edge : current.getAdjacent()) {
-                adjacent = edge.getTo();
-                bw.append("{\"from\":" + current.getId() + ",\"to\":" + adjacent.getId() + ",\"color\":\"gray\"},");
+            // {"from":{"x": 0, "y": 3},"to":{ "x": 0, "y": 150.0}}
+            for(Edge edge : from.getAdjacent()) {
+                to = edge.getTo();
+                toX = to.getX() / maxX * 960.0;
+                toY = to.getY() / maxY * 500.0;
+                bw.append("{\"from\":{\"x\": " + fromX + ", \"y\": " + (-(fromY - 500)) + "},\"to\":{ \"x\": " + toX + ", \"y\": " + (-(toY - 500)) + "}},");
             }
         }
         
-        for(Edge edge : path) {
-            bw.append("{\"from\":" + edge.getFrom().getId() + ",\"to\":" + edge.getTo().getId() + ",\"color\":\"red\"},");
-        }
+        bw.append("],\"path\":[");
         
-        bw.write("]}");
+        for(Edge edge : path) {
+            from = edge.getFrom();
+            to = edge.getTo();
+            
+            fromX = from.getX() / maxX * 960.0;
+            fromY = from.getY() / maxY * 500.0;
+            toX = to.getX() / maxX * 960.0;
+            toY = to.getY() / maxY * 500.0;
+            
+            bw.append("{\"from\":{\"x\": " + fromX + ", \"y\": " + (-(fromY - 500)) + "},\"to\":{ \"x\": " + toX + ", \"y\": " + (-(toY - 500)) + "}},");
+        }
+
+        bw.write("],\"colors\":{\"edges\": \"gray\", \"path\": \"red\"}}");
         bw.flush();
         bw.close();
     }
-    
+
     public void printPath(Stack<Edge> path, Vertex origin, Vertex destination, long timeToRun) {
         Edge edge = null;
         System.out.println("===========================");
@@ -72,5 +84,5 @@ public class GraphPrinter {
         System.out.println(String.format("\n%.1f", destination.getSlot(Constants.DISTANCE, Double.class)));
         System.out.println(timeToRun);
     }
-    
+
 }
